0629a66ffb1006cd1e659ec6c573d8cc8ac29005
adamf0x <adamfox96@gmail.com>
2021-10-22 21:07:18 -0700

    installed and tested mocha framework

---
+++ testing_repo/test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Test run', function() {
+  describe('===', function() {
+    it('should return true when the values are equal', function() {
+      assert.equal(1===1, true);
+    });
+  });
+});
